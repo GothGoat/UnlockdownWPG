01eb2d90cfe6be57ac8a78163a6e41ad06430ef1
GothGoat <rio.imam@gmail.com>
2021-09-27 12:44:24 +0700

    Text Idle Kedap-Kedip

---
+++ Assets/Script/IdleDetect.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleDetect : MonoBehaviour
+{
+    public GameObject idleText;
+    int IdleTimeSetting = 4;
+    float LastIdleTime;
+
+    void Awake()
+    {
+        LastIdleTime = Time.time;
+    }
+
+    private void Update()
+    {
+        if (Input.anyKey)
+        {
+            LastIdleTime = Time.time;
+        }
+
+        if (IdleCheck() == true)
+        {
+            idleText.SetActive(true);
+            idleText.GetComponent<Animator>().enabled = true;
+        }
+        else
+        {
+            idleText.SetActive(false);
+            idleText.GetComponent<Animator>().enabled = false;
+        }
+    }
+
+    public bool IdleCheck()
+    {
+        return Time.time - LastIdleTime > IdleTimeSetting;
+    }
+}
